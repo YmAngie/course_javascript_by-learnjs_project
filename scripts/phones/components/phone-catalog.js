87ff8e1e82ea419b5bfd008df8432db2f6ff332a
YmAngie <a.ymydykova@gmail.com>
2018-08-05 18:21:48 +0300

    Fix formatting

--- scripts/phones/components/phone-catalog.js
+++ scripts/phones/components/phone-catalog.js
@@ -1,63 +1,57 @@
-'use strict'
+'use strict';
 
-import Component from '../../component.js'
+import Component from '../../component.js';
 
 export default class PhoneCatalog extends Component {
-  constructor({ element, phones }) {
-    super({ element })
+    constructor({element, phones}) {
+        super({element})
+        this._phones = phones;
 
-    this._phones = phones;
-    this._render();
+        this._render();
 
-    this.on('click', '[data-element="phone-link"]', (event) => {
-      let phoneLink = event.delegateTarget,
-          phoneElement = phoneLink.closest('[data-element="phone"]');
+        this.on('click', '[data-element="phone-link"]', (event) => {
+            let phoneLink = event.delegateTarget,
+                phoneElement = phoneLink.closest('[data-element="phone"]');
 
-      this._trigger('phoneSelected', phoneElement.dataset.phoneId);
-    });
+            this._trigger('phoneSelected', phoneElement.dataset.phoneId);
+        })
 
-    this.on('click', '[data-element="button-add"]', (event) => {
-      let addButton = event.delegateTarget,
-      phoneElement = addButton.closest('[data-element="phone"]');
+        this.on('click', '[data-element="button-add"]', (event) => {
+            let addButton = event.delegateTarget,
+                phoneElement = addButton.closest('[data-element="phone"]');
 
-      this._trigger('addToCart', phoneElement.dataset.phoneId);
-    });
-  }
+            this._trigger('addToCart', phoneElement.dataset.phoneId);
+        })
+    }
 
-  _render() {
-    this._element.innerHTML = `
-      <ul class="phones">
-        ${ this._phones.map(phone => `
-        
-          <li class="thumbnail" 
-              data-element="phone"
-              data-phone-id="${ phone.id }">
-            <a
-              href="#!/phones/${ phone.id }"
-              class="thumb"
-              data-element="phone-link"
-            >
-              <img alt="${ phone.name }" src="${ phone.imageUrl }">
-            </a>
-  
-            <div class="phones__btn-buy-wrapper">
-              <a class="btn btn-success" data-element="button-add">
-                Add
-              </a>
-            </div>
-  
-            <a 
-              href="#!/phones/${ phone.id }"
-              data-element="phone-link"
-            >
-              ${ phone.name }
-            </a>
-            
-            <p>${ phone.snippet }</p>
-          </li>
-        
-        `).join('') }
-      </ul>
-    `;
-  }
+    _render() {
+        this._element.innerHTML = `
+            <ul class="phones">
+                ${ this._phones.map(phone => `
+                    <li class="thumbnail" 
+                        data-element="phone"
+                        data-phone-id="${ phone.id }">
+                        <a href="#!/phones/${ phone.id }"
+                            class="thumb"
+                            data-element="phone-link">
+                            <img alt="${ phone.name }" src="${ phone.imageUrl }">
+                        </a>
+                    
+                        <div class="phones__btn-buy-wrapper">
+                          <a class="btn btn-success" data-element="button-add">
+                            Add
+                          </a>
+                        </div>
+                        
+                        <a href="#!/phones/${ phone.id }"
+                            data-element="phone-link">
+                            ${ phone.name }
+                        </a>
+                        
+                        <p>${ phone.snippet }</p>
+                    </li>
+                `).join('') }
+            </ul>
+        `;
+    }
 }
--- scripts/phones/components/shopping-cart.js
+++ scripts/phones/components/shopping-cart.js
@@ -3,7 +3,6 @@ import Component from '../../component.js';
 export default class ShoppingCart extends Component {
     constructor({element}) {
         super({element});
-
         this._items = {};
 
         this._render();
@@ -11,7 +10,7 @@ export default class ShoppingCart extends Component {
         this.on('click', '[data-element="button-remove"]', (event) => {
             let item = event.delegateTarget.dataset.item;
             this.removeItem(item);
-        });
+        })
     }
 
     addItem(item) {
@@ -53,9 +52,12 @@ export default class ShoppingCart extends Component {
                     <li>${item} (${quantity}) 
                         <button 
                             data-element="button-remove"
-                            data-item="${item}"
-                            >x</button></li>
-                    `).join('')}
+                            data-item="${item}">
+                            x
+                        </button>
+                    </li>
+                    `).join('')
+                }
             </ul>
         `;
     }
